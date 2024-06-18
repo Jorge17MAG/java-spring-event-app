68d55e0d33459d59b5cbcc81f258a258d1953621
Jorge Arellano <jorgemathi4s@gmail.com>
2024-06-18 11:26:54 -0500

    quitarPostController

--- src/main/java/com/idat/events/controller/UserController.java
+++ src/main/java/com/idat/events/controller/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +27,6 @@ public class UserController {
         return userService.getAllUsers();
     }
 
-    @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
 
         String userEmail = user.getEmail();
